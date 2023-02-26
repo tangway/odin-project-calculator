b7c26d072a35e5d804b87a194d836cdab26592b7
tangwwwei <20863759+tangwwwei@users.noreply.github.com>
2023-02-26 14:30:59 +0800

    Implement displayValue function on number pad

--- script.js
+++ script.js
@@ -1,54 +1,8 @@
-// DONE
-// clicking "plus" should store value1 and operator to be used in operate()
-// it should also indicate somewhere that the "plus" operator has been triggered
-// and then optionally clear the screen
-// 
-// then we can input the next number
-// clicking the "equals" div should store value2 and call operate() with the 3 
-// params
-// DONE
-
-// TODO
-// next is writing function that can accumulate the numbers until u 
-// press =
-  // this would mean u keep assigning prev results to value1?
-  // eg. 11 is already entered, then i click + then 11, then when i 
-  // click + again it should use operate function to get the result and 
-  // assign to value1
-  // maybe i should take out the operator call function?
-  // when clicking + after 2 numbers have been entered, how do i check
-  // that value1 or value2 exist already?
-
-// figure out the logic flow for displaying value2 in box for value2
-
-// DONE
-// write the logic to store value1 and value2 and also to display them
-  // every number click should display AND store value1 and value2
-// and write a "C" div and function to clear all variables 
-// DONE
-
-// DONE
-// it might be easier to rewrite the logic for + and = rather than
-// to improve on it since i keep getting confused by it
-// DONE
-
-// NEXT
-// figure out how to For example, 12 + 7 - 5 * 3 = should yield 42. 
-// An example of the behavior we’re looking for would be this student 
-// solution.
-  // this means if value1Store and value2Store and operator are all
-  // already assigned values, clicking another operator again shd
-  // perform the equivalent of the equals function
-  //
-  // have to modify equalsOperation() so that when i press 1 + 2 + 3
-  // it clears the display when i press 3 after the 2nd +
-
-// NEXT
-// You should round answers with long decimals so that they don’t 
-// overflow the screen.
-
-
 let operator = undefined
+let value1Store = undefined
+let value2Store = undefined
+let result = undefined
+let operatorCounter = 0
 
 const operatorsMap = {
   "+": (a, b) => a + b,
@@ -63,12 +17,20 @@ const operate = function (operator, value1, value2) {
 
 const equalsOperation = function () {
   if (result) {
-    result = operate(operator, Number(value2Store), Number(result))
+    result = operate(operator, Number(result), Number(value2Store))
   } else {
     result = operate(operator, Number(value1Store), Number(value2Store))
   }
   console.log(`result is: ${result}`)
   display.value = result
+}
+
+const displayValue = function (digit) {
+  if (Number(display.value) === result) {
+    display.value = digit
+  } else {
+    display.value = display.value + digit;
+  }
 }
 
 // let value1Display = document.querySelector('input[name="value1"]');
@@ -85,10 +47,6 @@ const minus = document.querySelector("#minus");
 const multiply = document.querySelector("#multiply");
 const divide = document.querySelector("#divide");
 const equals = document.querySelector("#equals");
-
-let value1Store = undefined
-let value2Store = undefined
-let result = undefined
 
 const storeValue = function (num) {
   if (value1Store === undefined) {
@@ -107,36 +65,42 @@ const storeValue = function (num) {
     console.log(`inside (operator && value2Store === undefined)`);
     value2Store = num;
   } else if (value2Store && !result) {
-    // when value2Store already is assigned so u just add on the 
+    // when value2Store already is assigned so u just add on the
     // numbers
     console.log(`inside else if (value2Store && !result)`);
-    value2Store = value2Store + num;}
-  // } else if (value2Store.length >= 1 && result) {
-  //   // when value2Store already is assigned a digit and there is 
-  //   // already a result, meaning this is the 3rd number entered, add 
-  //   // on to it
-  //   value2Store = value2Store + num} 
-    else if (value2Store && result) {
-    value2Store = num
-  };
-  
-  console.log(`value1Store is currently: ${value1Store}`);
-  console.log(`value2Store is currently: ${value2Store}`);
+    value2Store = value2Store + num;
+  } else if (operatorCounter > 1 && result) {
+    // when value2Store already is assigned a digit and there is
+    // already a result, meaning this is the 3rd number entered, add
+    // on to it
+    console.log(`inside else if (operatorCounter > 1 && result)`)
+    value2Store += num;
+  } else if (value2Store && result) {
+    console.log(`inside else if (value2Store && result)`);
+    value2Store = value2Store + num;
+  }
+
+  console.log(`value1Store is ${value1Store}`);
+  console.log(`value2Store is ${value2Store}`);
 }
 
 div1.addEventListener("click", () => {
   storeValue("1")
-  display.value = display.value + "1";
+  displayValue("1")
+  // if (Number(display.value) === result) {
+  //   display.value = "1"
+  // } else {
+  //   display.value = display.value + "1";
+  // }
 });
 div2.addEventListener("click", () => {
   storeValue("2")
-  display.value = display.value + "2";
+  displayValue("2")
 });
 div3.addEventListener("click", () => {
   storeValue("3")
-  display.value = display.value + "3";
+  displayValue("3")
 });
-
 
 
 /*THE OPERATORS */
@@ -146,8 +110,9 @@ clear.addEventListener("click", () => {
   value1Store = undefined
   value2Store = undefined
   result = undefined
+  operatorCounter = 0
   display.value = ""
-  console.log(`shit is cleared and reset to default state`)
+  console.log(`shit been reset to default state`)
 })
 
 plus.addEventListener("click", () => {
@@ -156,11 +121,14 @@ plus.addEventListener("click", () => {
   if (value1Store && value2Store && operator) {
     console.log(`inside plus 1st if`)
     equalsOperation()
-    display.value = ""
+    value2Store = ""
+    display.value = result
   } else {
     operator = "+";
     display.value = "";  
   }
+  
+  operatorCounter += 1
 });
 
 minus.addEventListener("click", () => {

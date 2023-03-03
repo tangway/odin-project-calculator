954bb0a85e597593935e7a2e9f63bd7504adbf8d
tangwwwei <20863759+tangwwwei@users.noreply.github.com>
2023-03-03 15:05:14 +0800

    Implement lastButtonPressedCheck() and operatorGuardClauses()

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let operator = undefined
 let value1Store = undefined
 let value2Store = undefined
 let result = undefined
-let equalsPressed = false
+let lastButtonPressed = undefined
 let operatorCounter = 0
 
 const operatorsMap = {
@@ -40,6 +40,7 @@ const displayValue = function (digit) {
 let display = document.querySelector('input[name="equalDisplay"]');
 // let operatorDisplay = document.querySelector('input[name="operator"]')
 
+const numperPad = document.querySelector("#numberPad")
 const div1 = document.querySelector("#number1");
 const div2 = document.querySelector("#number2");
 const div3 = document.querySelector("#number3");
@@ -57,7 +58,7 @@ const multiply = document.querySelector("#multiply");
 const divide = document.querySelector("#divide");
 const equals = document.querySelector("#equals");
 
-const storeValue = function (num) {
+const storeValue = function(num) {
   if (value1Store === undefined) {
     // when value1Store has nothing yet
     console.log(`inside if (value1Store === undefined)`);
@@ -96,136 +97,139 @@ const storeValue = function (num) {
   console.log(`value2Store is ${value2Store}`);
 }
 
-// const storeValue = function (num) {
-//   switch (true) {
-//     case value1Store === undefined:
-//       console.log(`inside if (value1Store === undefined)`);
-//       value1Store = num;
-//       break;
-//     case !operator:
-//       console.log(`inside else if (!operator)`);
-//       value1Store = value1Store + num;
-//       break;
-//     case operator && value2Store === undefined:
-//       console.log(`inside (operator && value2Store === undefined)`);
-//       value2Store = num;
-//       break;
-//     case value2Store && !result:
-//       console.log(`inside else if (value2Store && !result)`);
-//       value2Store = value2Store + num;
-//       break;
-//     case operatorCounter > 1 && result:
-//       console.log(`inside else if (operatorCounter > 1 && result)`);
-//       value2Store += num;
-//       break;
-//     case value2Store && result:
-//       console.log(`inside else if (value2Store && result)`);
-//       value2Store = value2Store + num;
-//       break;
-//   }
-//   console.log(`value1Store is ${value1Store}`);
-//   console.log(`value2Store is ${value2Store}`);
-// }
+const lastButtonPressedCheck = function() {
+  if (lastButtonPressed === "=") {
+    clearFunc()
+  } else {
 
+  }
+}
+
+const clearFunc = function() {
+  operator = undefined
+  value1Store = undefined
+  value2Store = undefined
+  result = undefined
+  lastButtonPressed = undefined
+  operatorCounter = 0
+  display.value = ""
+  console.log(`shit been reset to default state`)
+}
+
+// numperPad.addEventListener("click", () => {
+//   if (lastButtonPressed === "=") {
+//     operator = undefined
+//     value1Store = undefined
+//     value2Store = undefined
+//     result = undefined
+//     lastButtonPressed = undefined
+//     operatorCounter = 0
+//     console.log(`cleared cos of equals`)
+//   }
+// });
 
 div1.addEventListener("click", () => {
-  storeValue("1")
-  displayValue("1")
-  // if (Number(display.value) === result) {
-  //   display.value = "1"
-  // } else {
-  //   display.value = display.value + "1";
-  // }
+  lastButtonPressedCheck();
+  storeValue("1");
+  displayValue("1");
 });
 div2.addEventListener("click", () => {
-  storeValue("2")
-  displayValue("2")
+  lastButtonPressedCheck();
+  storeValue("2");
+  displayValue("2");
 });
 div3.addEventListener("click", () => {
-  storeValue("3")
-  displayValue("3")
+  lastButtonPressedCheck();
+  storeValue("3");
+  displayValue("3");
 });
 div4.addEventListener("click", () => {
-  storeValue("4")
-  displayValue("4")
+  lastButtonPressedCheck();
+  storeValue("4");
+  displayValue("4");
 });
 div5.addEventListener("click", () => {
-  storeValue("5")
-  displayValue("5")
+  lastButtonPressedCheck();
+  storeValue("5");
+  displayValue("5");
 });
 div6.addEventListener("click", () => {
-  storeValue("6")
-  displayValue("6")
+  lastButtonPressedCheck();
+  storeValue("6");
+  displayValue("6");
 });
 div7.addEventListener("click", () => {
-  storeValue("7")
-  displayValue("7")
+  lastButtonPressedCheck();
+  storeValue("7");
+  displayValue("7");
 });
 div8.addEventListener("click", () => {
-  storeValue("8")
-  displayValue("8")
+  lastButtonPressedCheck();
+  storeValue("8");
+  displayValue("8");
 });
 div9.addEventListener("click", () => {
-  storeValue("9")
-  displayValue("9")
+  lastButtonPressedCheck();
+  storeValue("9");
+  displayValue("9");
 });
 div0.addEventListener("click", () => {
-  storeValue("0")
-  displayValue("0")
+  lastButtonPressedCheck();
+  storeValue("0");
+  displayValue("0");
 });
 
 
 /*THE OPERATORS */
 
-clear.addEventListener("click", () => {
-  operator = undefined
-  value1Store = undefined
-  value2Store = undefined
-  result = undefined
-  equalsPressed = false
-  operatorCounter = 0
-  display.value = ""
-  console.log(`shit been reset to default state`)
-})
+clear.addEventListener("click", clearFunc)
 
-plus.addEventListener("click", () => {
-  // if value1Store and value2Store and operator are all
-  // already assigned values then run equalsOperation
-  
+const operatorGuardClauses = function(op) {
   if (Number(display.value) === result) {
-    operator = "+";
+    operator = op;
+    // display.value = "";
   } else if (value1Store && value2Store && operator) {
     console.log(`inside else if (value1Store && value2Store && operator)`);
     equalsOperation();
     value2Store = "";
     display.value = result;
   } else {
-    operator = "+";
+    operator = op;
     display.value = "";
   }
+}
 
+plus.addEventListener("click", () => {
+  // if value1Store and value2Store and operator are all
+  // already assigned values then run equalsOperation
+  
+  operatorGuardClauses("+")
   operatorCounter += 1;
+  lastButtonPressed = "+";
 
 });
 
 minus.addEventListener("click", () => {
-  operator = "-";
-  display.value = "";
+  operatorGuardClauses("-")
+  operatorCounter += 1;
+  lastButtonPressed = "-";
 });
 
 multiply.addEventListener("click", () => {
-  operator = "*";
-  display.value = "";
+  operatorGuardClauses("*")
+  operatorCounter += 1;
+  lastButtonPressed = "*";
 });
 
 divide.addEventListener("click", () => {
-  operator = "/";
-  display.value = "";
+  operatorGuardClauses("/")
+  operatorCounter += 1;
+  lastButtonPressed = "/";
 });
 
 equals.addEventListener("click", () => {
-  equalsPressed = true
   equalsOperation()
+  lastButtonPressed = "="
 })
 
 
